735cc8d1211d3901e5182a1e07757600d6886cc8
Vincent615 <73717292+Vincent615@users.noreply.github.com>
2020-12-20 19:46:18 -0600

    Create gears.py

---
+++ gears.py
@@ -0,0 +1,29 @@
+# Describe the Wooden Sword through a dictionary
+wSword = {
+    'descript': 'a wooden weapon for newbies',
+    'damage': 5,
+    }
+
+# Describe the Iron Sword through a dictionary
+iSword = {
+    'descript': 'an iron weapon for pros',
+    'damage': 10,
+    }
+
+# Describe the Diamond Sword through a dictionary
+dSword = {
+    'descript': 'a diamond weapon for experts',
+    'damage': 15,
+    }
+
+# Describe the Iron Armor through a dictionary
+iArmor = {
+    'descript': 'a great suit of gears for a pro',
+    'resistance': 3,
+    },
+
+# Describe the Diamond Armor through a dictionary
+dArmor = {
+    'descript': 'a great suit of gears for an expert',
+    'resistance': 8,
+    }
